c592a7b12c0f7fb1a4cbd6f349925b25dafd8301
Minionguyjpro <minionguyjpro@gmail.com>
2023-07-08 13:53:03 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -2,12 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { exit } = require('process');
 
+const options = core.getInput('options');
 const path = core.getInput('path');
 
 var exec = require('child_process').exec;
 
 if (process.platform === 'win32') {
-exec(`"%PROGRAMFILES(X86)%\\Inno Setup 6\\iscc.exe" "${path}"`,
+exec(`"%PROGRAMFILES(X86)%\\Inno Setup 6\\iscc.exe" ${options} "${path}"`,
     function (error, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
