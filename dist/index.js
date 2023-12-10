b43d320db8ccb0d0605fe88a96970c36cbb0e581
Minionguyjpro <minionguyjpro@gmail.com>
2023-12-10 14:29:47 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -12,7 +12,7 @@ var exec = require('child_process').exec;
 if (process.platform === 'win32') {
   if (fs.existsSync(process.env.GITHUB_WORKSPACE)) {
     console.log(`Path used: "${workspacePath}\\${path}"`);
-    exec(`"%PROGRAMFILES(X86)%\\Inno Setup 6\\iscc.exe" ${options} "${workspacePath}\\${path}"`, function (error, stdout, stderr) {
+    exec(`"%PROGRAMFILES(X86)%\\Inno Setup 6\\iscc.exe" ${options} "${workspacePath}\\${path}"`, { stdio: 'ignore' }, function (error, stdout, stderr) {
       console.log(stdout);
       console.log(stderr);
       if (error) {
