741d87648571df6959860a5b9af74a0c75372ce0
Minionguyjpro <minionguyjpro@gmail.com>
2023-12-11 15:25:41 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ const path = core.getInput('path');
 var exec = require('child_process').exec;
 
 if (process.platform === 'win32') {
-  if (workspacePath && workspacePath.trim() !== '') {
+  if (workspacePath && fs.existsSync(workspacePath) && fs.lstatSync(workspacePath).isDirectory()) {
     exec(`"%PROGRAMFILES(X86)%\\Inno Setup 6\\iscc.exe" ${options} "${workspacePath}\\${path}"`, { stdio: 'ignore' }, function (error, stdout, stderr) {
       console.log(stdout);
       console.log(stderr);
