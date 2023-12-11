aa53f5f6fdadb2917b294fb01797f2c40c575c81
Minionguyjpro <minionguyjpro@gmail.com>
2023-12-11 16:43:04 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,28 @@
 const fs = require('fs');
 const core = require('@actions/core');
+// eslint-disable-next-line no-unused-vars
+const github = require('@actions/github');
 
 const workspacePath = process.env.GITHUB_WORKSPACE;
 const options = core.getInput('options');
 const path = core.getInput('path');
 
+var exec = require('child_process').exec;
+
 if (process.platform === 'win32') {
-  try {
-    if (fs.existsSync(workspacePath) && fs.readdirSync(workspacePath).length > 0) {
-      const { stdout, stderr } = require('child_process').execSync(`"%PROGRAMFILES(X86)%\\Inno Setup 6\\iscc.exe" ${options} "${workspacePath}\\${path}"`, { stdio: 'pipe' });
+  if (fs.existsSync(process.env.GITHUB_WORKSPACE) && fs.readdirSync(workspacePath).length > 0) {
+    exec(`"%PROGRAMFILES(X86)%\\Inno Setup 6\\iscc.exe" ${options} "${workspacePath}\\${path}"`, { stdio: 'ignore' }, function (error, stdout, stderr) {
       console.log(stdout);
-      if (stderr) {
-        throw new Error(stderr);
+      if (error) {
+        core.setFailed(stderr);
+        process.exit(error.code || 1);
       }
-    } else {
-      throw new Error('The repository was not cloned. Please specify the actions/checkout action before this step.');
-    }
-  } catch (error) {
-    core.setFailed(error.message);
+    });
+  } else {
+    core.setFailed('Error: The repository was not cloned. Please specify the actions/checkout action before this step.');
+    process.exit(error.code || 1);
   }
 } else {
-  core.setFailed('This action is only supported on Windows!');
+  core.setFailed('Error: This action is only supported on Windows!');
+  process.exit(error.code || 1);
 }
