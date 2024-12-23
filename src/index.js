c1b61a348f487c23a81da19cecb9911602337f1b
Minionguyjpro <minionguyjpro@gmail.com>
2024-12-23 13:58:44 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,18 +25,14 @@ async function run() {
       if (workspaceExists && workspaceNotEmpty) {
         // Escaping quotes in the options array
         const escapedOptions = options.map((str) =>
-          str.replace(/(["'])/g, "\\$1"),
+          str.replace(/(["'])/g, '$1'));
         );
-
-        // Debugging output to check the escaped options
-        console.log("Escaped Options:", escapedOptions);
 
         execFile(
           `${process.env["ProgramFiles(x86)"]}\\Inno Setup 6\\iscc.exe`,
           [...escapedOptions, `${workspacePath}\\${path}`],
           (execError, stdout, stderr) => {
-            console.log("stdout:", stdout);
-            console.log("stderr:", stderr);
+            console.log(stdout, stderr);
             if (execError) {
               core.setFailed(`Execution failed with error: ${stderr}`);
               process.exit(execError.code || 1);
