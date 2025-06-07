7c668b985a983ecc48732b2ed2cbc2d803d2831b
Minionguyjpro <minionguyjpro@gmail.com>
2025-06-07 18:04:53 +0200

    Update and rename .eslintrc.yml to eslint.config.js

---
+++ .github/linters/eslint.config.js
@@ -0,0 +1,59 @@
+import js from "@eslint/js";
+import github from "eslint-plugin-github";
+import jest from "eslint-plugin-jest";
+import babelParser from "@babel/eslint-parser";
+import prettier from "eslint-plugin-prettier";
+
+// ESLint flat config expects an array of config objects
+export default [
+  {
+    files: ["**/*.js", "**/*.mjs", "**/*.cjs"],
+    ignores: [
+      "!.*",
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/coverage/**",
+      "*.json",
+    ],
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: "module",
+      globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+      },
+      parser: babelParser,
+      parserOptions: {
+        ecmaVersion: 2023,
+        sourceType: "module",
+        requireConfigFile: false,
+        babelOptions: {
+          babelrc: false,
+          configFile: false,
+          presets: ["jest"],
+        },
+      },
+    },
+    plugins: {
+      github,
+      jest,
+      prettier,
+    },
+    rules: {
+      camelcase: "off",
+      "eslint-comments/no-use": "off",
+      "eslint-comments/no-unused-disable": "off",
+      "i18n-text/no-en": "off",
+      "import/no-commonjs": "error", // Enforce ES modules only
+      "import/no-namespace": "off",
+      "no-console": "off",
+      "no-unused-vars": "off",
+      "prettier/prettier": "error",
+      semi: "off",
+    },
+    settings: {},
+  },
+  js.configs.recommended,
+  github.configs.recommended,
+  jest.configs.recommended,
+];
