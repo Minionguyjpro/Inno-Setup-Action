ec32ae4d3b82296c50915f06afefc946935ce933
Minionguyjpro <minionguyjpro@gmail.com>
2025-06-07 19:18:36 +0200

    Update eslint.config.js

--- .github/linters/eslint.config.js
+++ .github/linters/eslint.config.js
@@ -8,21 +8,16 @@ import jest from "eslint-plugin-jest";
 import babelParser from "@babel/eslint-parser";
 import prettier from "eslint-plugin-prettier";
 
-// Make __dirname available
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Setup FlatCompat to use legacy shareable configs if needed
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// ESLint flat config expects an array of config objects
 export default [
-  // FlatCompat usage to extend from old-style configs, if needed
-  ...compat.extends("eslint:recommended"),
+  ...compat.extends("prettier"),
 
-  // Direct flat config for your JS files
   {
     files: ["**/*.js", "**/*.mjs", "**/*.cjs"],
     ignores: [
@@ -35,11 +30,17 @@ export default [
     languageOptions: {
       ecmaVersion: 2023,
       sourceType: "module",
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-env"],
+        },
+      },
       globals: {
         Atomics: "readonly",
         SharedArrayBuffer: "readonly",
       },
-      parser: babelParser,
     },
     plugins: {
       github,
@@ -51,17 +52,15 @@ export default [
       "eslint-comments/no-use": "off",
       "eslint-comments/no-unused-disable": "off",
       "i18n-text/no-en": "off",
-      "import/no-commonjs": "error", // Enforce ES modules only
+      "import/no-commonjs": "error",
       "import/no-namespace": "off",
       "no-console": "off",
       "no-unused-vars": "off",
       "prettier/prettier": "error",
       semi: "off",
     },
-    settings: {},
   },
 
-  // Recommended configs from plugins/libraries
   js.configs.recommended,
   github.configs.recommended,
   jest.configs.recommended,
